6ab388018fac8263cf31fc0b085c090f87a0447e
Tomas Salgado <salgadotomas@outllok.com>
2016-11-04 12:48:48 -0300

    bootstrap y modificacion vista de servicios

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -1,17 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <div class="container" >
+        <div class="row" >
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">Servicios</div>
 
                     <div class="panel-body">
-                        You are logged in!
+                        <div class="row">
+                            <div class="col-xs-6 col-md-3">
+                                <a href="{{ url('/home') }}" class="thumbnail">
+                                    <img src="img/home-bg.jpg" alt="">
+                                </a>
+                            </div>
+                            Cirugia canina
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-md-3">
+                                <a href="#" class="thumbnail">
+                                    <img src="img/home-bg.jpg" alt="...">
+                                </a>
+                            </div>
+                            ...
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
